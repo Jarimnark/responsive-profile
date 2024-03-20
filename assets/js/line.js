506d5e13eceace65288c09c8b8f9c322323bb10b
Jarimnark <jarimnark@gmail.com>
2024-03-20 22:41:42 +0700

    fix the social media

---
+++ assets/js/line.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // rest: true // Animation repeat
+});
+
+sr.reveal(".projects__card", { interval: 100 });
